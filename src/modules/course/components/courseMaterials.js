aed6a1827072f2d1f7c299463d5c5581f3f3673d
Your Name <you@example.com>
2021-05-27 15:29:26 +0800

    revised course materials

--- src/modules/course/components/courseMaterials.js
+++ src/modules/course/components/courseMaterials.js
@@ -5,8 +5,8 @@ const CourseMaterials = ({ courseMaterials }) => {
   const { getUser } = useContext(UserContext);
   const user = getUser();
 
-  const handleClick = async (event) => {
-
+  const handleClick = async (event, material) => {
+    window.open(`http://localhost:1337${material.url}`)
   }
 
   return (
@@ -14,10 +14,12 @@ const CourseMaterials = ({ courseMaterials }) => {
         {
             courseMaterials?
             <div>
-                {courseMaterials.map((material)=>
-                <div key={material.id} >
-                    <div className="text-2xl border-b-4">{material.title}</div>
-                    <div className="py-6">{material.materialDescription}</div>
+                {courseMaterials.map((courseMaterial)=>
+                <div key={courseMaterial.id} >
+                    <div className="text-2xl border-b-4">{courseMaterial.title}</div>
+                    <div className="py-6">{courseMaterial.materialDescription}</div>
+                    {/* <a href={`http://localhost:1337${courseMaterial.material.url}`} download="proposed_file_name">Download</a> */}
+                    <button onClick={(event)=>handleClick(event, courseMaterial.material)}>Download</button>
                 </div>
                 )}
             </div>
