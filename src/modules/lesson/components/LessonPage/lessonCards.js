89da9c84ecd2e800ebe08e55522b07b78079f5d3
Winnie Wong <winnie@shergon.com>
2021-07-10 13:16:35 +0800

    user profile icon fix & lessonCard max width

--- src/modules/lesson/components/LessonPage/lessonCards.js
+++ src/modules/lesson/components/LessonPage/lessonCards.js
@@ -3,19 +3,29 @@ import { UserContext } from '../../../../contexts/UserContext';
 import ForwardIcon from '@material-ui/icons/Forward';
 import { Link } from 'react-router-dom';
 import CheckIcon from '@material-ui/icons/Check';
-import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(theme => ({
+  lessonTextContainer: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+  },
+  lessonText: {
+    display: 'flex',
+  },
+}));
 
-const LessonCards = ({ lessonsDetail, progressHandler, purchased }) => {
+const LessonCards = ({ lessonsDetail }) => {
   const { getUser } = useContext(UserContext);
   const user = getUser();
+  const classes = useStyles();
 
   return (
     <>
       <div>
-        <div className="rounded-t mb-0 py-3 border-b-2">
+        <div className="py-3 mb-0 border-b-2 rounded-t">
           <div className="flex flex-wrap items-center">
-            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+            <div className="relative flex-1 flex-grow w-full max-w-full px-4">
               <h3
                 className={
                   "font-semibold text-lg "
@@ -28,33 +38,33 @@ const LessonCards = ({ lessonsDetail, progressHandler, purchased }) => {
         </div>
         <div className="block w-full overflow-x-auto">
           <div>
-            {lessonsDetail.length === 0 && <p className='ml-4 mt-6 text-xs'>此課程暫時沒有資料</p>}
+            {lessonsDetail.length === 0 && <p className='mt-6 ml-4 text-xs'>此課程暫時沒有資料</p>}
             {
               lessonsDetail.map((lesson, ind) =>
-                <div className='border-gray-300 border-2 rounded-md m-4' key={ind}>
-                  <div className="px-6 py-4 text-xs whitespace-nowrap text-left flex items-center justify-between">
+                <div className='m-4 border-2 border-gray-300 rounded-md' key={ind}>
+                  <div className="flex items-center justify-between px-6 py-4 text-xs text-left whitespace-nowrap">
                     <div className='flex'>
                       {/* lesson status */}
                       {(user.id && user.id != "") &&
-                        <div type='button' className='w-10 h-10 rounded-full border-2 border-blueGray-50 shadow flex justify-center items-center'>
+                        <div type='button' className='flex items-center justify-center w-10 h-10 border-2 rounded-full shadow border-blueGray-50'>
                           {lesson.finished && <CheckIcon />}
                         </div>
                       }
                       {/* lesson name */}
-                      <div className='flex flex-col ml-3 w-24'>
+                      <div className='flex flex-col ml-3 w-96'>
                         <span
-                          className="font-bold text-lg">
+                          className="text-lg font-bold">
                           {lesson.title}
                         </span>
                         {/* lesson text */}
-                        <div className='overflow-visible'>
-                          {lesson.text}
+                        <div className='overflow-x-scroll'>
+                            {lesson.text}
                         </div>
                       </div>
                     </div>
                     {/* start lesson btn */}
                     <Link to={`/lesson/${lesson.id}`}>
-                      <button className='my-2 bg-gray-300 hover:bg-gray-400 text-gray-800 text-xs py-2 px-2 rounded inline-flex items-center'>
+                      <button className='inline-flex items-center px-2 py-2 my-2 text-xs text-gray-800 bg-gray-300 rounded hover:bg-gray-400'>
                         <span>
                           前往課程
                       </span>
