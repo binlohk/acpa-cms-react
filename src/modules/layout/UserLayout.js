516f75e512208c32bab9a40a1541ce59bb7c39be
Winnie-Wong-Binlo <winnie@binlo.io>
2021-08-18 10:24:11 +0800

    test

--- src/modules/layout/UserLayout.js
+++ src/modules/layout/UserLayout.js
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../layout/header'
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+
+
 const UserLayout = ({ children }) => {
+
+    const [showMobileWarning, setShowMobileWarning] = useState(false)
+
+    useEffect(() => {
+        if (window.innerWidth <= 1280)
+            setShowMobileWarning(true)
+    }, [])
 
     return (
         <>
@@ -11,6 +26,19 @@ const UserLayout = ({ children }) => {
                 <div className={`pt-24 h-full`}>
                     {children}
                 </div>
+                <Dialog
+                    open={showMobileWarning}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Let Google help apps determine location. This means sending anonymous location data to
+                            Google, even when no apps are running.
+                        </DialogContentText>
+                    </DialogContent>
+                </Dialog>
             </main>
 
         </>
