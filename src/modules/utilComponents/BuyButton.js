7b9b989c3fb628a7ec00842abcab263aac32056e
Meet Patel <meetcpatel906@gmail.com>
2022-03-31 11:34:18 +0530

    set loading on enrol button

--- src/modules/utilComponents/BuyButton.js
+++ src/modules/utilComponents/BuyButton.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { handleBuy, handleFreeApply } from '../../services/stripe'
 
-const BuyButton = ({ courseId, coursePrice, children }) => {
+const BuyButton = ({ courseId, coursePrice, children, setIsLoading, isDisabled }) => {
+    console.log("setIsLoading in buy button",setIsLoading);
     return (
         <div>
             {(coursePrice === 0) ?
                 (
                     <button
-                        style={{ background: '#235789', marginTop: 10 }}
-                        onClick={() => handleFreeApply(courseId)} className="inline-flex items-center justify-center px-3 py-2 text-xs font-bold text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700">
+                    style={isDisabled ? { background:  '#cccccc', color: 'black', marginTop: 10 } :{ background:  '#235789', marginTop: 10 } }
+                        onClick={() => {handleFreeApply(courseId); setIsLoading(true);}} className="inline-flex items-center justify-center px-3 py-2 text-xs font-bold text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700">
                         {children}
                     </button>
                 ) : (
                     <button
-                        style={{ background: '#235789', marginTop: 10 }}
-                        onClick={() => handleBuy(courseId)} className="inline-flex items-center justify-center px-3 py-2 text-xs font-bold text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700">
+                        disabled={isDisabled}
+                        style={isDisabled ? { background:  '#cccccc', color: 'black', marginTop: 10 } :{ background:  '#235789', marginTop: 10 } }
+                        onClick={() => { handleBuy(courseId); setIsLoading(true);}} className="inline-flex items-center justify-center px-3 py-2 text-xs font-bold text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700">
+                        
                         {children}
                     </button>
                 )
