74994242c2c5367070f0edd48b5166628b2932ca
Ashwinlab <ashwin.pradeep1397@gmail.com>
2022-04-20 14:49:59 +0530

    Carousel responsive fix

--- src/modules/course/components/AllCourses/CourseTitle.js
+++ src/modules/course/components/AllCourses/CourseTitle.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CourseTitle({ children }) {
     return (
         <>
-            <div className='text-4xl font-bold pl-12 pt-4 text-gray-200'>{children}</div>
+            <div className='text-4xl font-bold pl-6 md:pl-12 pt-4 text-gray-200'>{children}</div>
             <div class="text-start">
                 <span class="inline-block w-full h-1 rounded-full bg-gray-500 px-4 ml-1"></span>
             </div>
